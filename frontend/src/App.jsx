9d2f7240cd6670738a9993ee384be886ba9ec7d5
rhovelazquez <rocyvelazquez2021@gmail.com>
2024-10-16 20:13:45 -0300

    Se agrego estilos para la vista del frontend, para una mejor visualizacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import io from 'socket.io-client'
 import { useState,useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const socket= io("/")
 
@@ -27,22 +29,30 @@ function App() {
 
   const mensajesRecibido= (mensaje)=> setMensajes((state)=>[...state,mensaje] )//no reinicia cada vez que se envie el msj
 
-  return(
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Escribe tu mensaje' onChange={(e)=>setMensaje(e.target.value)}></input>
-        <button>Enviar</button>
+    return (
+      <div className="d-flex container-xxl justify-content-center align-items-center vh-100 bg-dark" style={{ }}>
+    <div className="p-4 bg-secondary text-light rounded" style={{ maxWidth: '600px', width: '100%' }}>
+      <form className="d-flex mb-3" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control me-2 bg-light text-dark border-0"
+          placeholder="Escribe tu mensaje"
+          onChange={(e) => setMensaje(e.target.value)}
+        />
+        <button className="btn btn-success">Enviar</button>
       </form>
 
-
-      <ul>
-        {
-          mensajes.map((mensaje,i)=>(
-            <li key={i}>{mensaje.from}:{mensaje.data}</li>
-          ))
-        }
+      <ul className="list-group">
+        {mensajes.map((mensaje, i) => (
+          <li key={i} className="list-group-item bg-light text-dark border-0 mb-2">
+            <strong>{mensaje.from}:</strong> {mensaje.data}
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
-export default App
+  </div>
+    );
+  };
+  
+
+export default App;
